415e8c716aa91a96f94e1680776ea2cdf7e97ba9
nilesh.v <nilesh.v@covrize.com>
2022-11-16 16:33:25 +0530

    integrate adminlte template

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <div>
+            <footer className="main-footer">
+                <strong>Copyright © 2021-2022 <a href="https://www.covrize.com/">CovRize</a>.</strong>
+                COLLECTIVE,WE RISE
+                <div className="float-right d-none d-sm-inline-block">
+                    <b>Version</b> 3.2.0
+                </div>
+            </footer>
+        </div>
+
+    );
+}
+
+export default Footer;
